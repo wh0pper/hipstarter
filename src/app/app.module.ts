8ffebf150b952ee7849b770616ef73cd1b5626c7
Jared Clemmensen and Sara Hamilton <jared@>
2018-04-04 10:18:27 -0700

    add additional project components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectService } from './project.service';
+import { ProjectEditComponent } from './project-edit/project-edit.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectAddComponent } from './project-add/project-add.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectListComponent
+    ProjectListComponent,
+    ProjectEditComponent,
+    ProjectDetailComponent,
+    ProjectAddComponent
   ],
   imports: [
     BrowserModule
